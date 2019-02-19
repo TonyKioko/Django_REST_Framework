308d2a7f69ec84fa6b87f198b5eb710abe342eb6
TonyKioko <tonykioko384@gmail.com>
2019-02-19 17:40:56 +0300

    create review api view added

--- courses/urls.py
+++ courses/urls.py
@@ -7,4 +7,7 @@ urlpatterns = [
     path('<int:pk>/', 
         views.RetrieveUpdateDestroyCourse.as_view(), 
         name='course_detail'),
+    path('<course_pk>/reviews/',
+        views.ListCreateReview.as_view(), 
+        name='review_list'),
 ]
--- courses/views.py
+++ courses/views.py
@@ -13,10 +13,21 @@ from . import serializers
 class ListCreateCourse(generics.ListCreateAPIView):
     queryset=models.Course.objects.all()
     serializer_class = serializers.CourseSerializer
-    
+
 class RetrieveUpdateDestroyCourse(generics.RetrieveUpdateDestroyAPIView):
     queryset = models.Course.objects.all()
     serializer_class = serializers.CourseSerializer   
+
+class ListCreateReview(generics.ListCreateAPIView):
+    queryset=models.Review.objects.all()
+    serializer_class = serializers.ReviewSerializer
+
+    def get_queryset(self):
+        return self.queryset.filter(course_id=self.kwargs.get('course_pk'))
+    
+class RetrieveUpdateDestroyReview(generics.RetrieveUpdateDestroyAPIView):
+    queryset = models.Review.objects.all()
+    serializer_class = serializers.ReviewSerializer  
 
 # class ListCreateCourse(APIView):
 #     def get(self,request,format=None):
