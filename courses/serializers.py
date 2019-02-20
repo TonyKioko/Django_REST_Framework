fd01c0443ed1f1bc44e68dcd50293ef25f42a3f5
TonyKioko <tonykioko384@gmail.com>
2019-02-20 17:06:35 +0300

    rating validator added

--- courses/serializers.py
+++ courses/serializers.py
@@ -6,9 +6,9 @@ from . import models
 
 class ReviewSerializer(serializers.ModelSerializer):
     class Meta:
-        # extra_kwargs = {
-        #     'email': {'write_only': True}
-        # }
+        extra_kwargs = {
+            'email': {'write_only': True}
+        }
         fields = (
             'id',
             'course',
@@ -20,11 +20,11 @@ class ReviewSerializer(serializers.ModelSerializer):
         )
         model = models.Review
         
-    # def validate_rating(self, value):
-    #     if value in range(1, 6):
-    #         return value
-    #     raise serializers.ValidationError(
-    #         'Rating must be an integer between 1 and 5')
+    def validate_rating(self, value):
+        if value in range(1, 6):
+            return value
+        raise serializers.ValidationError(
+            'Rating must be an integer between 1 and 5')
         
         
 class CourseSerializer(serializers.ModelSerializer):
