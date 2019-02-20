52bd4ce86631229dd16a44a3055520a9505f5e2a
TonyKioko <tonykioko384@gmail.com>
2019-02-20 13:26:39 +0300

    switching between between token and session based authentication

--- ed_reviews/settings.py
+++ ed_reviews/settings.py
@@ -39,6 +39,8 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
 
     'rest_framework',
+    'rest_framework.authtoken',
+
     'courses',
 ]
 
@@ -121,7 +123,9 @@ USE_TZ = True
 
 REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': (
-        'rest_framework.authentication.TokenAuthentication',
+        'rest_framework.authentication.SessionAuthentication',
+
+        # 'rest_framework.authentication.TokenAuthentication',
     ),
     'DEFAULT_PERMISSION_CLASSES': (
         'rest_framework.permissions.IsAuthenticatedOrReadOnly',
